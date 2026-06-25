e23c296e858724e6426a97c79eb70a457d2a836e
Nagaram Chakradhar Singh <chakri.august2006@gmail.com>
2026-06-25 07:31:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "Chakri@2006"
+MYSQL_DB = "survey_db"
+SECRET_KEY = "your_secret_key_here"  # Move this to config.py, use a long random string
